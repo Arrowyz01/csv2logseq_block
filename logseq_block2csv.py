3ab6c6a45551f9de903fcfa136508440d8c3b865
Arrowyz01 <85311840+Arrowyz01@users.noreply.github.com>
2022-02-05 21:10:26 +0100

    Add files via upload

--- logseq_block2csv.py
+++ logseq_block2csv.py
@@ -4,38 +4,48 @@ import pandas as pd
 # create a new dict
 dict_data={}
 
+list1 = ['1','1','2','2','1','1']
+
+# delete adjacent duplicate 
+def del_adjacent(alist):
+    for i in range(len(alist) - 1, 0, -1):
+        if alist[i] == alist[i-1]:
+            del alist[i]
+
 # syntax
 symb1 = '-'
 symb2 = '::'
 # open a md file
 with open(sys.argv[1],'r')as file:
   # read everyline in file
-  for line in file:
-    # for line with '-', and without 'query' or '.csv', we put it in the dict and give it a key 'title' 
-    if symb1 in line[:2]:
-        if 'query' in line:
-            continue
-        elif '.csv' in line:
-            continue
-        # when meet a blank line, skip
-        elif line[2:].count('\n') == len(line[2:]):
-            continue
+    for line in file:
+        # for line with '-', and without 'query' or '.csv', we put it in the dict and give it a key 'title' 
+        if symb1 in line.strip()[:2]:
+            if 'query' in line:
+                continue
+            elif '.csv' in line:
+                continue
+            # when meet a blank line, skip
+            elif line.strip()[2:].count('\n') == len(line.strip()[2:]):
+                continue
+            else:
+                dict_data.setdefault('title',[]).append(line.strip()[2:].strip(" #]\n").replace('[',''))
+        # for line with ':: ', and without 'query' or '.csv', we treat the word before '::' as key, the words after '::' as value
+        elif symb2 in line:
+            if 'query' in line:
+                continue
+            elif '.csv' in line:
+                continue
+            else:
+                for kv in [line.strip().split('::')]:
+                    if len(kv) != 1:
+                        dict_data.setdefault(kv[0],[]).append(kv[1].strip())
+                    else:
+                        dict_data.setdefault(kv[0],[]).append('')
         else:
-            dict_data.setdefault('title',[]).append(line[2:].strip(" #]\n").replace('[',''))
-    # for line with ':: ', and without 'query' or '.csv', we treat the word before '::' as key, the words after '::' as value
-    elif symb2 in line:
-        if 'query' in line:
             continue
-        elif '.csv' in line:
-            continue
-        else:
-            for kv in [line.strip().split('::')]:
-                if len(kv) != 1:
-                    dict_data.setdefault(kv[0],[]).append(kv[1].strip())
-                else:
-                    dict_data.setdefault(kv[0],[]).append('')
-    else:
-        continue
+
+del_adjacent(dict_data['title'])
 #print(dict_data)
 
 # read all the keys as a list
